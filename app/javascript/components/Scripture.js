a80c4bba7fcde63474801a7c77f6d04b789cc729
Minh Huynh <minhh1990@gmail.com>
2018-08-18 18:22:55 -0700

    added env management and proxy server for cors preflight handling

--- app/javascript/components/Scripture.js
+++ app/javascript/components/Scripture.js
@@ -2,20 +2,22 @@ import React from "react"
 import PropTypes from "prop-types"
 class Scripture extends React.Component {
 
+ state = {verses: ""}
+
  componentDidMount(){
 				 this.getVerses();
  }
 
  getVerses(){
-		fetch('https://bibles.org/v2/chapters/eng-NASB:Exod.2/verses.js', {
+		fetch('https://cors-anywhere.herokuapp.com/https://bibles.org/v2/chapters/eng-NASB:Exod.2/verses.js', {
 					  headers: {
-										'Authorization': "Basic " + btoa(ENV["bible_api_token"]),
-										'Content-Type': 'application/json'
-										 },
+										'Authorization': "Basic " + btoa(process.env.BIBLE_API_KEY + ":X")
+										 }
 						})
 		.then(response => response.json())
 		.then(data => {
-						console.log(data)
+						console.log(data);
+						this.setState({verses: data.response.verses[0]});
 		});
 	}
 
